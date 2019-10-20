1d0f474bfd01d10e750517120e884d6a0459f320
Ben Tapfuma <takutapfu@gmail.com>
2019-10-20 16:49:40 +0200

    removing redux-logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,11 @@ import rootReducer from './root-reducer';
 import { persistStore} from 'redux-persist'; //allow the browser to store the cache
 
 //the middleware that the store is expecting from redux, is going to be an array.
-const middlewares = [logger];
+const middlewares = [];
+//removing redux-logger from production build
+if(process.env.NODE_ENV === 'development'){ //NODE_ENV - set by react. either development,production or test
+    middlewares.push(logger);
+}
 
 //Create the store object, instantiated with createStore
 //createStore, function that will get the root-reducer and the return value of applyMiddleware
@@ -18,4 +22,6 @@ export const persistor = persistStore(store); //a persisted version of the state
 
 
 export default ({store,persistor});
-
+//When actions come through, being dispatched by the action creaters, they hit and update the reducer but along the way they also pass the middleware that is applied thru applyMiddleware all.
+//middlewares - functions that catch action and do something and return the actions and continues down to update the reducers
+// the logger is a middleware that catches the action and logs it to us. B4 and after the state is updated.
